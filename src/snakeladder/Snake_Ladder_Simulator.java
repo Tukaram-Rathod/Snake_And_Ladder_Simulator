66ba6ac8cd218ed39158b4afb36f289fd1be0f43
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-31 17:52:40 +0530

    Added. welcome message for snake and ladder simulator

---
+++ src/snakeladder/Snake_Ladder_Simulator.java
@@ -0,0 +1,10 @@
+package snakeladder;
+
+public class Snake_Ladder_Simulator {
+
+	public static void main(String[] args) {
+		System.out.println("***Welcome To Snake And Ladder Simulator***");
+
+	}
+
+}
