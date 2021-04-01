7d2e521e9fbb1fe8b8f30c0764059202de04b100
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-01 10:11:27 +0530

    Added.count number of dice roll and position

--- src/snakeladder/Snake_Ladder_Simulator.java
+++ src/snakeladder/Snake_Ladder_Simulator.java
@@ -4,32 +4,51 @@ public class Snake_Ladder_Simulator {
 
 	public static void main(String[] args) {
 		int position = 0;
+		int DiceRollCount = 0;
 		final int WINNING_POSITION = 100;
+		System.out.println("Initial Position ==>" +position);
 		//using while loop
 		while(position <= WINNING_POSITION) {	
 			Random rand = new Random();
 			int diceValue = rand.nextInt(6)+1;
 			System.out.println("Dice Value is ==> " +diceValue);
 			int option = rand.nextInt(3);
+			//check condition in switch
+			DiceRollCount++;
+			if(position == WINNING_POSITION) {
+				System.out.println("<====YOU WON THE GAME====>");
 			
-			if(option == 0) {
-				System.out.println("#NO PLAY");
+			break;
 			}
-			else if(option == 1) {
-				System.out.println("#LADDER");
-				position += diceValue;
-				System.out.println("Current Position is ==>" + position);
-			}
-			else if(option == 2) {
-				System.out.println("#SNAKE");
-				position -= diceValue;
-				System.out.println("Current Position is ==>" + position);	
-				
-				if(position < 0) {
+			switch(option) {
+				case 0:
+					System.out.println("#NO PLAY");
+					break;
+				case 1:
+					System.out.println("#LADDER");
+					position += diceValue;
+					System.out.println("Current Position is ==>" + position);
+					
+					if(position > 100) { 
+						position = position - diceValue;
+					System.out.println("Current Position is ==>" + position);	
+					}
+					break;
+				case 2:
+					System.out.println("#SNAKE");
+					position -= diceValue;
+					System.out.println("Current Position is ==>" + position);
+					
+					if(position < 0) { 
 						position = 0;
-						System.out.println("Current Position is ==>" + position);	
-				}
+					System.out.println("Current Position is ==>" + position);	
+					}
+					break;
+				default:
+					System.out.println("NOT CORRECT");
 			}
+			System.out.println("Dice Roll Count ===>"+DiceRollCount);
+
 		}
 	}
 }
