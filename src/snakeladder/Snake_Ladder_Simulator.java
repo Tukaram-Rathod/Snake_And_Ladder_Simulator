b6d4cb675c862b7c83d1b7879c796c4a8b204649
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-01 20:53:59 +0530

    Added. impliment code for two player

--- src/snakeladder/Snake_Ladder_Simulator.java
+++ src/snakeladder/Snake_Ladder_Simulator.java
@@ -2,63 +2,106 @@ package snakeladder;
 import java.util.Random;
 public class Snake_Ladder_Simulator {
 
-	static int player1;
-	static int player2;
 	public static void main(String[] args) {
-			System.out.println("player1:");
-			Player();
-			System.out.println("player2:");
-			Player();
-		}
-
-	static void Player() {
-		
-		int position = 0;
-		int DiceRollCount = 0;
+		//variables
+		int positionplayer1 = 0;
+		int positionplayer2 = 0;
+		int counterP1 = 0;
+		int counterP2 = 0;
 		final int WINNING_POSITION = 100;
-		System.out.println("Initial Position ==>" +position);
-		while(position <= WINNING_POSITION) {	
+	
+		//using while loop
+		while(positionplayer1 <= WINNING_POSITION  && positionplayer2 <= WINNING_POSITION) {	
 			Random rand = new Random();
-			int diceValue = rand.nextInt(6)+1;
-			System.out.println("Dice Value is ==> " +diceValue);
+			int diceValue1 = rand.nextInt(6)+1;
+			System.out.println("PLAYER1 --> ");
+			System.out.println("Dice Value is ==> " +diceValue1);
 			int option = rand.nextInt(3);
 			//check condition in switch
-			DiceRollCount++;
-			if(position == WINNING_POSITION) {
-				System.out.println("<====YOU WON THE GAME====>");
-			
-			break;
-			}
+			counterP1++;
+			if(positionplayer1 == WINNING_POSITION) {
+				System.out.println("<==============>PLAYER1 WON THE GAME<===============>");
+				break;
+				}
 			switch(option) {
 				case 0:
 					System.out.println("#NO PLAY");
 					break;
 				case 1:
 					System.out.println("#LADDER");
-					position += diceValue;
-					System.out.println("Current Position is ==>" + position);
+					positionplayer1 += diceValue1;
+					System.out.println("Current Position is ==>" + positionplayer1);
 					
-					if(position > 100) { 
-						position = position - diceValue;
-					System.out.println("Current Position is ==>" + position);	
+					if(positionplayer1 > 100) { 
+						positionplayer1 = positionplayer1 - diceValue1;
+					System.out.println("Current Position is ==>" + positionplayer1);	
 					}
 					break;
 				case 2:
 					System.out.println("#SNAKE");
-					position -= diceValue;
-					System.out.println("Current Position is ==>" + position);
+					positionplayer1 -= diceValue1;
+					System.out.println("Current Position is ==>" + positionplayer1);
 					
-					if(position < 0) { 
-						position = 0;
-					System.out.println("Current Position is ==>" + position);	
+					if(positionplayer1 < 0) { 
+						positionplayer1 = 0;
+					System.out.println("Current Position is ==>" + positionplayer1);	
 					}
 					break;
 				default:
 					System.out.println("NOT CORRECT");
 			}
-			System.out.println("Dice Roll Count ===>"+DiceRollCount);
-	
+			System.out.println("Dice Roll Count for Player1 ===>"+counterP1);
+			System.out.println("-----------------------------------------------------");
+		
+		//player2
+			
+			Random rand2 = new Random();
+			int diceValue2 = rand.nextInt(6)+1;
+			System.out.println("PLAYER2 --> ");
+			System.out.println("Dice Value is ==> " +diceValue2);
+			int option2 = rand.nextInt(3);
+			counterP2++;
+			if(positionplayer2 == WINNING_POSITION){
+				System.out.println("<==============>PLAYER2 WON THE GAME<===============>");
+				break;
+				}
+			switch(option2) {
+				case 0:
+					System.out.println("#NO PLAY");
+					break;
+				case 1:
+					System.out.println("#LADDER");
+					positionplayer2 += diceValue2;
+					System.out.println("Current Position is ==>" + positionplayer2);
+					
+					if(positionplayer2 > 100) { 
+						positionplayer2 = positionplayer2 - diceValue2;
+					System.out.println("Current Position is ==>" + positionplayer2);	
+					}
+					break;
+				case 2:
+					System.out.println("#SNAKE");
+					positionplayer2 -= diceValue2;
+					System.out.println("Current Position is ==>" + positionplayer2);
+					
+					if(positionplayer2 < 0) { 
+						positionplayer2 = 0;
+					System.out.println("Current Position is ==>" + positionplayer2);	
+					}
+					break;
+				default: 
+					System.out.println("NOT CORRECT");
+				
+			}
+			System.out.println("Dice Roll Count for Player2 ===>"+counterP2);
+			System.out.println("-------------------------------------------------");
+			
 		}
-	
-	}
+		
+		if(positionplayer1==WINNING_POSITION) 
+			System.out.println("***PLAYER 1 WON THE GAME***");
+		else
+			System.out.println("***PLAYER 2 WON THE GAME***");
+	}	
 }
+	
